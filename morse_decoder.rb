a9b89ef3b7e88fe9cc56362438c62754c418d737
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-03-17 22:38:20 +0200

    Complete the algorithm

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -26,3 +26,25 @@
   '-.--' => 'Y',
   '--..' => 'Z'
 }
+
+def get_letter(letter)
+    @current_letter = @dictionary[letter]
+    @current_letter
+  end
+  
+  def decode_word(word)
+    @letters = word.split
+    @current_word = ''
+    @letters.each do |letter|
+      get_letter(letter)
+      @current_word += get_letter(letter)
+    end
+    print "#{@current_word} "
+  end
+  
+  def decode_message(message)
+    @words = message.split('   ')
+    @words.each do |word|
+      decode_word(word)
+    end
+  end
